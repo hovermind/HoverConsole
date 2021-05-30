3bbe0bed909f3b297e12b5d6baafab919ccbbcbd
HASSAN ハッサン <console041@gmail.com>
2021-05-30 20:01:18 +0900

    Add DI project

---
+++ ConsoleApp.DependencyInjection/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace ConsoleApp.Simple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            var services = Startup.ConfigureServices();
+            var serviceProvider = services.BuildServiceProvider();
+
+            serviceProvider.GetService<EntryPoint>().Run(args);
+        }
+    }
+}
---
+++ ConsoleApp.DependencyInjection/Services/IFoo.cs
@@ -0,0 +1,7 @@
+﻿namespace ConsoleApp.Simple.Services
+{
+    public interface IFoo
+    {
+        int GetRandomNumber(int start = 1, int end = 10);
+    }
+}
